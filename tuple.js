819b3959d7a9379d88310ad1deb32dd105bfcdba
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-24 23:33:00 +0300

    Finalized on tuples

---
+++ tuple.js
@@ -0,0 +1,6 @@
+// tuples unlike arrays can store objects of different type
+var variables = [];
+variables[0] = "Helloo";
+variables[1] = 2;
+console.log(variables);
+console.log(typeof variables);
---
+++ tuple.ts
@@ -0,0 +1,10 @@
+// tuples unlike arrays can store objects of different type
+
+let variables = [];
+
+variables[0] = "Helloo";
+variables[1] = 2;
+
+console.log(variables);
+
+console.log(typeof variables);
