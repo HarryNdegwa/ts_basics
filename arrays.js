92b4f00b8bd32ff9acc5f732a7c975f3fe5e5e1e
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-24 23:28:00 +0300

    Finalized on arrays

---
+++ arrays.js
@@ -0,0 +1,5 @@
+var names = ["Harry", "Mike", "Kelvin"];
+console.log(names);
+var numbers = new Array(10);
+numbers[0] = 1;
+console.log(numbers);
---
+++ arrays.ts
@@ -0,0 +1,9 @@
+let names: string[] = ["Harry", "Mike", "Kelvin"];
+
+console.log(names);
+
+let numbers: number[] = new Array(10);
+
+numbers[0] = 1;
+
+console.log(numbers);
