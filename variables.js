cc95e487720ed190cbf86ad909926465cf2b7b28
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-24 21:37:50 +0300

    Variables done

---
+++ variables.js
@@ -0,0 +1,6 @@
+var firstName = "Harry";
+var age = 21;
+var isUser = true;
+var user;
+var lastName = "Mike";
+console.log(firstName, age, isUser, lastName);
---
+++ variables.ts
@@ -0,0 +1,7 @@
+let firstName: string = "Harry";
+let age: number = 21;
+let isUser: boolean = true;
+let user;
+let lastName = "Mike";
+
+console.log(firstName, age, isUser, lastName);
