9b7fe78e69357d6797d48ef571626d6ff21d1f57
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-24 22:08:50 +0300

    Finalized loop

---
+++ loops.js
@@ -0,0 +1,14 @@
+var times = 10;
+while (times > 0) {
+    if (times == 4) {
+        console.log("Times is 4");
+        break;
+    }
+    console.log("Yeyyy!");
+    times--;
+}
+var timess = 10;
+do {
+    console.log("World");
+    timess--;
+} while (timess > 0);
---
+++ loops.ts
@@ -0,0 +1,17 @@
+let times: number = 10;
+
+while (times > 0) {
+  if (times == 4) {
+    console.log("Times is 4");
+    break;
+  }
+  console.log("Yeyyy!");
+  times--;
+}
+
+let timess: number = 10;
+
+do {
+  console.log("World");
+  timess--;
+} while (timess > 0);
