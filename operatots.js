12fd8a17cf7764aed65571f6ec40b5c1f0110632
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-24 21:54:04 +0300

    Finalized ts operators

---
+++ operatots.js
@@ -0,0 +1,7 @@
+var a = 2;
+var b = 5;
+b++;
+a--;
+var greaterThanOne = a > 1 ? true : false;
+console.log(greaterThanOne);
+console.log(a + b);
---
+++ operatots.ts
@@ -0,0 +1,11 @@
+let a: number = 2;
+let b: number = 5;
+
+b++;
+a--;
+
+let greaterThanOne: boolean = a > 1 ? true : false;
+
+console.log(greaterThanOne);
+
+console.log(a + b);
