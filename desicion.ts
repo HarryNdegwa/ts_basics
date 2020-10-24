ce19685bf5826807cdc8cbae21a856d8cf803d14
HarryNdegwa <harrisonndegwa65@gmail.com>
2020-10-24 22:00:08 +0300

    Finalized ts decision making

---
+++ desicion.ts
@@ -0,0 +1,17 @@
+let isAuthenticated: boolean = false;
+
+if (isAuthenticated) {
+  console.log("You are authenticated");
+} else {
+  console.log("You are not authenticated");
+}
+
+let amount: number = 100;
+
+if (amount > 100) {
+  console.log("You can have an iphone with this");
+} else if (amount == 100) {
+  console.log("Hey you, add some more to get an iphone");
+} else {
+  console.log("It seems you cant have an iphone");
+}
